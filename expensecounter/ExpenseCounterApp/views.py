d3a6b2b49b3dafb1ebf49cbb680e13ab3719e98d
Chris <chriswhotakesphotos@gmail.com>
2022-10-05 21:35:33 -0400

    Date selector works

--- expensecounter/ExpenseCounterApp/views.py
+++ expensecounter/ExpenseCounterApp/views.py
@@ -14,17 +14,22 @@ def index(request):
     
     two_weeks_ago_interval = datetime.timedelta(days=14)
     two_weeks_ago = timezone.now() - two_weeks_ago_interval
-    date_range_start = two_weeks_ago
-    date_range_end = timezone.now()
+    date_range_start = format_date_for_html(two_weeks_ago)
+    date_range_end = format_date_for_html(timezone.now())
+    
+    #date_range_start_formatted = str(date_range_start.year) + "-" + str(date_range_start.month) + "-" + str(date_range_start.day)
+    #date_range_start_formatted = str(date_range_start.year) + "-" + str(date_range_start.month) + "-" + str(date_range_start.day)
+    #date_range_start_formatted = format_date_for_html(date_range_start)
+    #print("The: " + date_range_start_formatted)
     
     #ordered_db = expense_item.objects.order_by('name')
     #ordered_db = expense_item.objects.all()
     ordered_db = expense_item.objects.filter(purchase_time__range=[date_range_start, date_range_end])
     context = {'expense_items': ordered_db,
-                'worked': "No POST data was sent",
+                'worked': "Default value",
                 'app_title': appTitle,
                 'date_range_start': date_range_start,
-                'date_range_end': date_range_end}
+                'date_range_end': date_range_end,}
     
     if request.method == "POST":
         if request.POST.get('name'):
@@ -42,9 +47,36 @@ def index(request):
             return HttpResponse(template.render(context, request))
             
         if request.POST.get('startDate'):
-            context['date_range_start'] = request.POST.get('startDate')
-            context['date_range_end'] = request.POST.get('endDate')
+        #format_date_for_html isn't necessary here because the dates are being read from the form in the POST data, so they're already formatted correctly
+            date_range_start = request.POST.get('startDate')
+            date_range_end = request.POST.get('endDate')
+            
+            ordered_db = expense_item.objects.filter(purchase_time__range=[date_range_start, date_range_end])
+            context['date_range_start'] = date_range_start
+            context['date_range_end'] = date_range_end
+            context['expense_items'] = ordered_db
+            
+            print("Start date:" + date_range_start)
+            print("End date:" + date_range_end)
             context['worked'] = "Date range POST data was sent"
             return HttpResponse(template.render(context, request))
     
+    context['worked'] = "No POST data was sent"
     return HttpResponse(template.render(context, request))
+    
+#Use this to format dates before being sent to a template for use in a date input
+#This prevents us from having to treat dates differently whether they're passed to a template as a
+#python datetime or as already formatted for HTML (ie if they came from POST data & were then sent
+#back to another template)
+def format_date_for_html(date):
+    month = str(date.month)
+    if len(month) == 1:
+        month = str(0) + month
+    
+    day = str(date.day)
+    if len(day) == 1:
+        day = str(0) + day
+    
+    date_formatted = str(date.year) + "-" + month + "-" + day
+    return date_formatted
+    
