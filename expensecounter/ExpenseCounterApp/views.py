fc1d79371412c339b4cf7903abdc8f1607f83054
Chris <chriswhotakesphotos@gmail.com>
2022-10-06 21:04:56 -0400

    Some work done on sorting

--- expensecounter/ExpenseCounterApp/views.py
+++ expensecounter/ExpenseCounterApp/views.py
@@ -1,4 +1,5 @@
 import datetime
+from decimal import *
 
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
@@ -59,10 +60,14 @@ def index(request):
             print("Start date:" + date_range_start)
             print("End date:" + date_range_end)
             context['worked'] = "Date range POST data was sent"
+            
+            get_percents(date_range_start, date_range_end)
+            
             return HttpResponse(template.render(context, request))
     
     context['worked'] = "No POST data was sent"
     return HttpResponse(template.render(context, request))
+    
     
 #Use this to format dates before being sent to a template for use in a date input
 #This prevents us from having to treat dates differently whether they're passed to a template as a
@@ -79,4 +84,25 @@ def format_date_for_html(date):
     
     date_formatted = str(date.year) + "-" + month + "-" + day
     return date_formatted
+
+
+#Get an array of the percentage breakdown of these categories:
+#food_grocery, food_takeout, food_convenience, one_time_important, one_time_unimportant
+#recurring_important, #recurring_unimportant
+def get_percents(date_range_start, date_range_end):
+    date_filtered_db = expense_item.objects.filter(purchase_time__range=[date_range_start, date_range_end])
+    total_spend = get_total_spend(date_range_start, date_range_end, expense_item)
     
+    percentage_food_grocery = get_total_spend(date_range_start, date_range_end, date_filtered_db.filter(category="food_grocery")) / total_spend * 100
+    #print (percentage_food_grocery)
+    
+#Get total spending for a given date range    
+def get_total_spend(date_range_start, date_range_end, source_database):
+    date_filtered_db = source_database.objects.filter(purchase_time__range=[date_range_start, date_range_end])
+    total = Decimal(0)
+    
+    for item in date_filtered_db:
+        print(str(item.name))
+        total += item.cost
+        
+    return total
